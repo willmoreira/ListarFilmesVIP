f559139332484254d7d8699da022aeb5eb92e6db
William Moreira <william.moreira3@fatec.sp.gov.br>
2023-06-26 16:59:43 -0300

    ajuste na label do btn do alerta na createLoginView

--- ListFilmsVIP/Scenes/CreateLogin/CreateLoginView.swift
+++ ListFilmsVIP/Scenes/CreateLogin/CreateLoginView.swift
@@ -73,7 +73,7 @@ class CreateLoginView: UIView {
     
     func showAlert(title: String, message: String, completion: (() -> Void)?) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+        let okAction = UIAlertAction(title: ProjectStrings.ok.localized, style: .default) { _ in
             completion?()
         }
         alertController.addAction(okAction)
